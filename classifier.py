0d8fe600f260d59b2afdfd0f03a240f2ff522301
Nidjay78 <122665959+Nidjay78@users.noreply.github.com>
2024-02-28 14:38:45 -0800

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -48,7 +48,8 @@ class BertSentimentClassifier(torch.nn.Module):
 
         # Create any instance variables you need to classify the sentiment of BERT embeddings.
         ### TODO
-        raise NotImplementedError
+        self.dropout = torch.nn.Dropout(config.hidden_dropout_prob)
+        self.classifier = torch.nn.Linear(self.bert.config.hidden_size, self.num_labels)
 
 
     def forward(self, input_ids, attention_mask):
@@ -57,7 +58,10 @@ class BertSentimentClassifier(torch.nn.Module):
         # HINT: You should consider what is an appropriate return value given that
         # the training loop currently uses F.cross_entropy as the loss function.
         ### TODO
-        raise NotImplementedError
+        outputs = self.bert(input_ids, attention_mask)
+        cls_output = outputs.last_hidden_state[:,0,:]
+        cls_output = self.dropout(cls_output)
+        logits = self.classifier(cls_output)
 
 
 
